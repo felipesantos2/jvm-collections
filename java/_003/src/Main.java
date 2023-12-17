9ee5d8bc526332df5bca9cdeb24f35b2f75ca6c4
Felipe Pinheiro <santospinheiro627@gmail.com>
2023-12-17 18:43:43 -0300

    up intellij

---
+++ java/_003/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = { 1,2,3,4 };
+
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.println("Indice -> " + i + " Valor -> " + arr[i]);
+        }
+
+        /*
+        * Java collections framework
+        * Conjunto de estruturas pré-prontas para armazenamento de objetos
+        * Uma coleção também é um objeto
+        * Algumas operações
+        *   - Adição
+        *   - Remoção
+        *   - Acesso
+        *   - Pesquisa
+        *   - etc
+        *
+        * Os três grandes tipos
+        * LISTA
+        * MAPAS
+        *
+        *
+        * A API COLLECTIONS possui várias classes que representam algumas estruturas de dados
+        *
+        */
+    }
+}
